aa838b5125ea6bbbbf9ea4726278133f530d2455
Mahmoud Khaled <mahmoud.k.nasr@gmail.com>
2018-07-15 22:59:37 +0200

    Add all the data frames that needs to be implemented

--- models/data_frames/data_frame.py
+++ models/data_frames/data_frame.py
@@ -10,15 +10,19 @@ from database import DatabaseBaseClass
 
 class DataFrame(DatabaseBaseClass):
 
+    # Value used to make DataFrame as an abstract class
     __metaclass__ = ABCMeta
+    # The table name needed by SQL Alchemy
     __tablename__ = 'data_frames'
 
+    # Mete data of the table and considered as data members for the DataFrame class
     id = Column(Integer, primary_key=True, autoincrement=True)
     time = Column(DateTime, default=datetime.datetime.utcnow())
     frame_id = Column(Integer)
     frame_value = Column(VARBINARY(8))
     lap_id = Column(Integer, ForeignKey("laps.id"), nullable=False)
 
+    # link the DataFrame to Lap Class
     lap = relationship("Lap", back_populates="data_frames")
 
     @abstractmethod
@@ -26,6 +30,7 @@ class DataFrame(DatabaseBaseClass):
         self.frame_id = frame_id
         self.frame_value = frame_value
 
+    # responsible for the representation of the data when printing the object
     @abstractmethod
     def __repr__(self):
         return "<User(record id='%d', time='%s', frame ID='%d', frame value='%s')>" \
